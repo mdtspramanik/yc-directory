4cddbf0387c95ce96a4acfe961310ea2ec6f3bde
mdtspramanik <189224128+mdtspramanik@users.noreply.github.com>
2025-06-03 22:50:35 +0600

    chore(yc-directory): add project files

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+import NavBar from "../Components/NavBar";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <main className="font-work-sans">
+        <NavBar />
+
+        {children}
+      </main>
+    </>
+  );
+}
